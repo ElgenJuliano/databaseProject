674c3a86b85923d5170fd46aa4dd91928fa41c3d
Elgen Juliano <elgjul05502@stud.noroff.no>
2026-04-20 13:50:45 +0200

    table created

---
+++ models/rate.js
@@ -0,0 +1,17 @@
+const db = require(".");
+
+module.exports = (sequelize, Sequelize) => {
+    const Rate = sequelize.define('Rate', {
+        Value: {
+            type: Sequelize.DataTypes.INTEGER,
+            validate: {
+                min: 1,
+                max: 5
+            }
+        }
+    },{
+        timestamps: false
+    });
+    return Rate
+}
+
